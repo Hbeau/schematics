5019ccce0c4b16530ed27cfd892e32ac28ba951b
mmmaxou <mmmaxou@yahoo.fr>
2016-06-09 00:33:53 +0200

    Ajustement du responsive design et ajout d'un cercle de connexion

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,43 +1,48 @@
 <?php $this->load->view('template/head'); ?>
-<header>
-    <div class="navbar navbar-inverse">
-        <div class="container header">
-            <div class="row logo">
-                <h1>Schematics</h1>
-            </div>
-            <div class="row searchline well well-sm">
-                <!--                      Inserer le lien vers home ici-->
-                <a href="<?php echo site_url("/schematics") ?>">
-                    <span class="glyphicon glyphicon-home col-lg-1"></span>
-                </a>
-                <div class="searchbar form-group col-lg-8">
-                    <input type="text" class="form-control" id="searchbar">
+    <header>
+        <div class="navbar navbar-inverse">
+            <div class="container header">
+                <div class="row logo">
+                    <h1>Schematics</h1>
+                </div>
+                <div class="row searchline well well-sm">
+
+                    <a href="<?php echo site_url(" /schematics ") ?>" class="col-sm-1">
+                        <span class="glyphicon glyphicon-home"></span>
+                    </a>
+
+                    <div class="searchbar form-group col-sm-8">
+                        <input type="text" class="form-control" id="searchbar">
+                    </div>
+
+                    <div class="dropdown col-sm-3">
+                        <button class="btn btn-default btn-block dropdown-toggle" type="button" data-toggle="dropdown">Menu <span class="caret"></span></button>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Lien 1</a></li>
+                            <li><a href="#">Lien 2</a></li>
+                            <li><a href="#">Lien 3</a></li>
+                        </ul>
+                    </div>
+
                 </div>
 
-                <div class="dropdown">
-                    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Menu <span class="caret"></span></button>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Lien 1</a></li>
-                        <li><a href="#">Lien 2</a></li>
-                        <li><a href="#">Lien 3</a></li>
-                    </ul>
+                <div class="round">
+                    <span class="glyphicon glyphicon-user"></span>
                 </div>
-
             </div>
-        </div>
-        <!--
+            <!--
 <span class="name">
 
 <?php
-if(isset($username)){
-    echo $username;
-    echo "<a href='".site_url("User/disconect")."' class='btn'>deconnexion</a>";
-}
-else{
-    $this->load->view('template/login.php');;
-}
+    if(isset($username)){
+        echo $username;
+        echo "<a href='".site_url("User/disconect")."' class='btn'>deconnexion</a>";
+    }
+                   else{
+                       $this->load->view('template/login.php');;
+                   }
 ?>
 </span>
 -->
-    </div>    
-</header>
+        </div>
+    </header>
