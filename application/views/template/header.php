65cd169abc938d113bde6e0063c373d210c2237a
mmmaxou <mmmaxou@yahoo.fr>
2016-06-08 22:20:04 +0200

    Ebauche header

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,38 +1,43 @@
 <?php $this->load->view('template/head'); ?>
-<body>
-
 <header>
+    <div class="navbar">
+        <div class="container">
+            <div class="row logo">
+                <div class="fake-logo"></div>
+            </div>
+            <div class="row searchline well well-sm">
+                <!--                      Inserer le lien vers home ici-->
+                <a href="#">
+                    <span class="glyphicon glyphicon-home col-lg-1"></span>
+                </a>
+                <div class="searchbar form-group col-lg-8">
+                    <input type="text" class="form-control" id="searchbar">
+                </div>
 
-    <div class="navbar navbar-inverse" >
-        <div class="nav-collapse">
+                <div class="dropdown">
+                    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown"><span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Lien 1</a></li>
+                        <li><a href="#">Lien 2</a></li>
+                        <li><a href="#">Lien 3</a></li>
+                    </ul>
+                </div>
 
-            <ul class="nav">
-                <li><a href="#">most recent</a></li>
-                <li><a href="#">most popular</a></li>
-                <li><a href="#">most downloaded</a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-            </ul>
-
+            </div>
         </div>
-        <span class="name">
+        <!--
+<span class="name">
 
-        <?php
-        if(isset($username)){
-           echo $username;
-            echo "<a href='".site_url("User/disconect")."' class='btn'>deconnexion</a>";
-        }
-        else{
-            $this->load->view('template/login.php');;
-        }
-        ?>
-        </span>
-    </div>
-
-
-
-   
-
+<?php
+if(isset($username)){
+    echo $username;
+    echo "<a href='".site_url("User/disconect")."' class='btn'>deconnexion</a>";
+}
+else{
+    $this->load->view('template/login.php');;
+}
+?>
+</span>
+-->
+    </div>    
 </header>
-
-
