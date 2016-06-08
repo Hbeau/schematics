2f0e5742cb1b92f1d60f596d8eb05cbad5631bf0
mmmaxou <mmmaxou@yahoo.fr>
2016-06-09 01:25:40 +0200

    Changement de la couleur principale en verte

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,36 +1,38 @@
 <?php $this->load->view('template/head'); ?>
-    <header>
-        <div class="navbar navbar-inverse">
-            <div class="container header">
-                <div class="row logo">
-                    <h1>Schematics</h1>
-                </div>
-                <div class="row searchline well well-sm">
-
-                    <a href="<?php echo site_url(" /schematics ") ?>" class="col-sm-1">
-                        <span class="glyphicon glyphicon-home"></span>
-                    </a>
-
-                    <div class="searchbar form-group col-sm-8">
-                        <input type="text" class="form-control" id="searchbar">
-                    </div>
-
-                    <div class="dropdown col-sm-3">
-                        <button class="btn btn-default btn-block dropdown-toggle" type="button" data-toggle="dropdown">Menu <span class="caret"></span></button>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Lien 1</a></li>
-                            <li><a href="#">Lien 2</a></li>
-                            <li><a href="#">Lien 3</a></li>
-                        </ul>
-                    </div>
-
-                </div>
-
-                <div class="round">
-                    <span class="glyphicon glyphicon-user"></span>
-                </div>
+<header>
+    <div class="navbar navbar-inverse">
+        <div class="container header">
+            <div class="row logo">
+                <h1>Schematics</h1>
             </div>
-            <!--
+            <div class="row searchline well well-sm">
+
+                <a href="<?php echo site_url("/schematics") ?>" class="col-sm-1">
+                    <span class="glyphicon glyphicon-home"></span>
+                </a>
+
+                <div class="searchbar form-group col-sm-8">
+                    <input type="text" class="form-control" id="searchbar">
+                </div>
+
+                <div class="dropdown col-sm-3">
+                    <button class="btn btn-default btn-block dropdown-toggle" type="button" data-toggle="dropdown">Menu <span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Lien 1</a></li>
+                        <li><a href="#">Lien 2</a></li>
+                        <li><a href="#">Lien 3</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div class="round">
+                <a href="#">
+                    <span class="glyphicon glyphicon-user"></span>
+                </a>
+            </div>
+        </div>
+        <!--
 <span class="name">
 
 <?php
@@ -44,5 +46,5 @@
 ?>
 </span>
 -->
-        </div>
-    </header>
+    </div>
+</header>
