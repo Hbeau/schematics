f86ebfb76e876d21d1441f1e2ee36846a50a4b35
mmmaxou <mmmaxou@yahoo.fr>
2016-06-11 00:21:11 +0200

    Grille pour les schemas; Mise en forme des schemas

--- application/views/template/schema.php
+++ application/views/template/schema.php
@@ -1,8 +1,13 @@
-<div class="schema">
-    <div class="images"></div>
-    <a href="#">
-        <h2 class="schemaTitle " data-toggle="modal" data-info="<?php echo $id?>" data-target="#myModal<?php echo $id?>">
-            <?php echo $name ?>
-        </h2>
+<div class="data">
+    <a href="#" class="schemaTitle " data-toggle="modal" data-info="<?php echo $id?>" data-target="#myModal<?php echo $id?>">
+        <div class="images">
+            <img src='http://lorempixel.com/350/350' />
+        </div>
+        <div class="vcenter">
+
+            <h2>
+                <?php echo $name ?>
+            </h2>
+        </div>
     </a>
 </div>
