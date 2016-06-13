0da9e793ec41ddf1f943ffff0125f7bc13df5ed7
mmmaxou <mmmaxou@yahoo.fr>
2016-06-13 23:14:05 +0200

    Interface qtip de connexion

--- application/views/template/SchemaModal.php
+++ application/views/template/SchemaModal.php
@@ -3,7 +3,7 @@
         <div class="modal-dialog">
 
             <!-- Modal content-->
-            <div class="modal-content">
+            <div class="modal-content capitalize">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                     <h4 class="modal-title" id="name"><?php echo $name ?></h4>
