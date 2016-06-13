9704ad27425914adf496296f62f8b981d1c73e85
mmmaxou <mmmaxou@yahoo.fr>
2016-06-14 00:03:56 +0200

    Debug affichage schemaModal

--- application/views/template/SchemaModal.php
+++ application/views/template/SchemaModal.php
@@ -15,45 +15,31 @@
                     <div id="myCarousel<?php echo $id?>" class="carousel slide" data-ride="carousel">
                         <!-- Indicators -->
                         <ol class="carousel-indicators">
+                          
                             <li data-target="#myCarousel<?php echo $id?>" data-slide-to="0" class="active"></li>
-                            <li data-target="#myCarousel<?php echo $id?>" data-slide-to="1"></li>
-                            <li data-target="#myCarousel<?php echo $id?>" data-slide-to="2"></li>
-                            <li data-target="#myCarousel<?php echo $id?>" data-slide-to="3"></li>
+                           
+                           <?php for($i=1; $i<count($images); $i++){
+    echo '<li data-target="#myCarousel<?php echo $id?>" data-slide-to="'.$i.'"></li>';
+} ?>
                         </ol>
 
                         <!-- Wrapper for slides -->
                         <div class="carousel-inner" role="listbox">
-
                             <?php  if(!empty($images)){
-
     foreach ($images as $img){
                             ?>
 
                             <div class="item">
-
-                                <img
-                                     src=" <?php echo site_url("uploads/schema/image/".$folder."/".$images[0])?>" alt="Chania" width="450" height="345">
+                                <img src="<?php echo site_url("uploads/schema/image/".$folder."/".$img)?>" alt="Chania" width="450" height="345">
                             </div>
 
                             <?php }}?>
-
-                            <div class="item">
-                                <img
-                                     src="http://static.planetminecraft.com/files/resource_media/screenshot/1317/dsadsadsadsadas_5327294.jpg" alt="Chania" width="450" height="345">
-                            </div>
-
-                            <div class="item">
-                                <img
-                                     src="http://t0.gstatic.com/images?q=tbn:ANd9GcS81NxcJWVDrCTf3mNGd-AmoDAvbuwlAVkzwdhp9iRo5hqz6Fk2IAZYf90"
-                                     alt="Flower" width="450" height="345">
-                            </div>
-
-                            <div class="item">
-                                <img
-                                     src="http://www.pcgamesn.com/sites/default/files/Hyperscale%20Minecraft%203.png"
-                                     alt="Flower" width="450" height="345">
-                            </div>
                         </div>
+
+                        <!--                        Ajout de la classe ACTIVE au premier item de la liste -->
+                        <script>
+                            $('.carousel-inner .item:first-child').addClass("active")
+                        </script>
 
                         <!-- Left and right controls -->
                         <a class="left carousel-control" href="#myCarousel<?php echo $id?>" role="button" data-slide="prev">
@@ -66,15 +52,17 @@
                         </a>
                     </div>
                 </div>
-                <p id="description"><?php echo $description?></p>
+                <p id="description">
+                    <?php echo $description?>
+                </p>
             </div>
             <div class="modal-footer">
 
-                <a id="linkTumbUp" href="<?php echo site_url("Schematics/tumb/".$id."/like") ?>"><span
-                                                                                                       class="glyphicon glyphicon-thumbs-up"></span><span id="tumbUp"><?php echo $like["like"]?></span></a>
-                <a id="linkTumbDown" href="<?php echo site_url("Schematics/tumb/".$id."/dislike") ?>"><span
-                                                                                                            class="glyphicon glyphicon-thumbs-down"></span><span id="tumbDown"><?php echo $like["dislike"]?></span></a>
-                <button type="button" class="btn btn-default my-style size18"><a href="ftp://173.137.63.1/home/schematics/<?php echo str_replace(" ","_",$name)?>.zip">Download</a></button>
+                <a id="linkTumbUp" href="<?php echo site_url(" Schematics/tumb/ ".$id."/like ") ?>"><span
+                                                                                                          class="glyphicon glyphicon-thumbs-up"></span><span id="tumbUp"><?php echo $like["like"]?></span></a>
+                <a id="linkTumbDown" href="<?php echo site_url(" Schematics/tumb/ ".$id."/dislike ") ?>"><span
+                                                                                                               class="glyphicon glyphicon-thumbs-down"></span><span id="tumbDown"><?php echo $like["dislike"]?></span></a>
+                <button type="button" class="btn btn-default my-style size18"><a href="ftp://173.137.63.1/home/schematics/<?php echo str_replace(" ","_ ",$name)?>.zip">Download</a></button>
 
 
             </div>
